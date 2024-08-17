b74e433cf1a04fc23f5e834f3f8b9cf088cfd7e2
Angelino Verhaeghe <verhaegheangelino@gmail.com>
2024-08-17 14:35:06 +0200

    Adding some starter packages to use when building our project

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,38 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
-})
+  compatibilityDate: "2024-04-03",
+  devtools: { enabled: true },
+  css: ["~/assets/css/main.css"],
+
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+
+  modules: [
+    "@nuxtjs/google-fonts",
+    "@nuxt/image",
+    "@nuxt/icon",
+    "@pinia/nuxt",
+    "@prisma/nuxt"
+  ],
+
+  googleFonts: {
+    families: {
+      Inter: true,
+      Lato: true,
+      Montserrat: true,
+      Roboto: true,
+    },
+  },
+
+  image: {
+    format: ["webp", "png", "jpg", "jpeg"],
+  },
+
+  icon: {
+    componentName: "NuxtIcon",
+  },
+});
